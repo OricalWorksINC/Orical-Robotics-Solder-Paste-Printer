d45c49521b279b96e3083bb90ddc5570ed67eeb2
Orical Incorporated <john@hobbywizards.com>
2016-07-06 15:38:34 -0400

    New Code! I found a new way to produce the same results in a shorter piece of code.
    New Code! I found a new way to produce the same results in a shorter
    piece of code.

--- Code/Solder_Paste_Printer/Solder_Paste_Printer.ino
+++ Code/Solder_Paste_Printer/Solder_Paste_Printer.ino
@@ -7,18 +7,21 @@
 #include <Stepper.h>
 
 const int ysteps = 200; // how many steps to travle end to end.
-const int zsteps = 200; // how many steps to travle end to end.
+const int cstepsout = 200; // how many steps to have the board exit the machine.
+const int cstepsin = 5;
 Stepper ystepper(ysteps, 8, 9, 10, 11);
-Stepper zstepper(zsteps, 2, 3, 4, 5);
+Stepper cstepperin(cstepsin, 2, 3, 4, 5);
+Stepper cstepperout(cstepsout, 2, 3, 4, 5);
 int inPin = 7;   // input pin for start button
 int val = 0;     // variable for reading the pin status
 int val2 = 0;
 int inPin2 = 6;
-int val3 = 0;
-int inPin3 = 3;
+int var = 0;
+int varpin = 3;
 void setup() {
  ystepper.setSpeed(100);
- zstepper.setSpeed(100);
+ cstepperin.setSpeed(100);
+ cstepperout.setSpeed(100);
  Serial.begin(9600);
 }
 
@@ -26,36 +29,17 @@ void loop() {
   Serial.println("Idle");
 val = digitalRead(inPin);  // read input value
 val2 = digitalRead(inPin2); 
-val3 = analogRead(inPin3); 
-if (val == LOW) {         // check if the input is LOW
+var = analogRead(varpin);
+if (val == HIGH) {         // check if the input is LOW
   Serial.println("Starting Print");
-zstepper.step(zsteps);
-delay(2000);
-if (val2 == LOW); {
+  if(val== HIGH){
+    while(var == LOW){
+cstepperin.step(cstepsin);
+}
+delay(1000);
 ystepper.step(ysteps);
-if (val3 == LOW);
-{ 
- zstepper.step(-zsteps);
- delay(1000);
- ystepper.step(-ysteps);
- delay(1000);
-  if (val3 == LOW);
-{ 
-zstepper.step(-zsteps);
- delay(1000);
- ystepper.step(-ysteps);
- delay(1000);
-if (val3 == LOW); {
-  zstepper.step(-zsteps);
- delay(1000);
- ystepper.step(-ysteps);
- delay(1000);
   }
-
-  }
-  }
-  }
-  }
+}
 else { Serial.println("timeout"); }
 
 
